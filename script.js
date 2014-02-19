e827be8c86c61677c107c61a952fb28860702382
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2014-02-19 22:20:50 +0000

    Minor coding style changes.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// prepare div
+var linkText = 'Скачать изображения';
+// Get all items for sale.
 var items = document.querySelectorAll("article.offer");
+// Generate div element, add event and populate them on the page.
 for (var i = 0; i < items.length; ++i) {
     var downloadDiv = document.createElement('div');
     downloadDiv.setAttribute('class', 'molotok_downloader');
     downloadDiv.setAttribute('data-id', items[i].getAttribute('data-id'));
-    downloadDiv.textContent = 'Download';
-    downloadDiv.addEventListener("click", processEvent, false);
+    downloadDiv.textContent = linkText;
+    downloadDiv.addEventListener('click', processEvent, false);
 
     // Insert next to .details element.
     var details = items[i].querySelector("div.details");
@@ -14,12 +16,15 @@ for (var i = 0; i < items.length; ++i) {
 
 function processEvent(e){
     e.stopPropagation();
+    // Retrieve item we need to download images for.
     var item = document.getElementById('item-' + e.target.getAttribute('data-id'));
-    eval('var photodata = ' + item.querySelector("div.photo").getAttribute('data-img'));
+    // Retrieve its data-img attribute content.
+    eval('var photodata = ' + item.querySelector('div.photo').getAttribute('data-img'));
     var re = /photos\/\w+\//;
+    // Download each image in its original size.
     for (var i = 0; i < photodata.length; ++i) {
         var url = photodata[i][0];
-        url = url.replace(re, "photos/oryginal/");
+        url = url.replace(re, 'photos/oryginal/');
         chrome.runtime.sendMessage({url: url});
     }
 }
