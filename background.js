ea8c4a1486e29b41349c0d5a1c5cd4de0dce4404
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2014-03-16 20:29:53 +0000

    Only modify downloads initiated by extension.

--- background.js
+++ background.js
@@ -25,13 +25,15 @@ chrome.extension.onMessage.addListener(
 });
 
 chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
-    var options = {
-        filename: item.filename,
-        conflict_action: 'overwrite',
-        conflictAction: 'overwrite'
-    };
-    if (localStorage.pathtosave.length) {
-        options.filename = localStorage.pathtosave + '/' +item.filename;
+    if (chrome.runtime.id === item.byExtensionId) {
+        var options = {
+            filename: item.filename,
+            conflict_action: 'overwrite',
+            conflictAction: 'overwrite'
+        };
+        if (localStorage.pathtosave.length) {
+            options.filename = localStorage.pathtosave + '/' +item.filename;
+        }
+        suggest(options);
     }
-    suggest(options);
 });
